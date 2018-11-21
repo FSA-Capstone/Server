595547631e75f595786bd6c54701d2c8b9e2cec9
Jarret Rose <jarretrose@gmail.com>
2018-11-21 14:24:14 -0500

    Basic setup for GrapheneDB hosted Neo4j server.

--- app.js
+++ app.js
@@ -3,25 +3,21 @@ const app = express();
 
 // setup Neo4j driver
 const neo4j = require('neo4j-driver').v1;
-const { graphenedbPass, graphenedbURL, graphenedbUser} = require('./environment')
+const { graphenedbPass, graphenedbURL, graphenedbUser } = require('./environment')
 
-// connect to Neo4j using the Bolt Protocol
-// Neo4j.auth.basic takes two parameters, a username and a password
+// connect to Neo4j
 const driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
-
 const session = driver.session();
 
 // Dummy Route - To test
-// testing connection to Neo4j by console logging user names
+// testing connection to Neo4j by console logging Admin node by name (dummy nodes)
 app.get('/', (req, res, next) => {
   session
-    .run(`MATCH(n:User) RETURN n.name LIMIT 25`)
-    .then(result =>
-      result.records.forEach(record => console.log(record._fields))
-    )
+    .run(`MATCH(n:Admin) RETURN n.name LIMIT 25`)
+    .then(result => result.records.forEach(record => console.log(record._fields)))
     .catch(err => console.log(err));
 
-    res.send("Hello World")
+    res.send('Hello World')
 });
 
 // error handling
